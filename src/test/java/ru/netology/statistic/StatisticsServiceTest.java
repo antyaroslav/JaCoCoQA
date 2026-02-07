9cdaff412add8253585946ceb8e357483e9dac63
Yaroslav Antonov <antoyaroslav@gmail.com>
2026-02-07 16:17:05 +0300

    Update StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -8,14 +8,12 @@ public class StatisticsServiceTest {
 
     @Test
     void shouldFindMaxWhenMaxAtBeginning() {
-        // Покрывает ветку: false (все элементы ≤ первого)
-        long[] incomes = {15, 12, 5, 8};
+               long[] incomes = {15, 12, 5, 8};
         assertEquals(15, service.findMax(incomes));
     }
 
     @Test
     void shouldFindMaxWhenMaxAtEnd() {
-        // Покрывает ветку: true (последний элемент > текущего максимума)
         long[] incomes = {5, 8, 4, 15};
         assertEquals(15, service.findMax(incomes));
     }
