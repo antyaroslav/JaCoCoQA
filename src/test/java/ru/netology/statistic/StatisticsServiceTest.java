8fa8ba869179a0d6bcb7055567d222ea38652f7b
antyaroslav <antoyaroslav@gmail.com>
2026-02-07 13:56:27 +0300

    JaCoCo 100

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,14 +6,52 @@ import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxWhenMaxAtBeginning() {
         StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
+        long[] incomes = {15, 12, 5, 8, 4}; // максимум в начале
+        long expected = 15;
+        long actual = service.findMax(incomes);
         Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMaxWhenMaxAtEnd() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {5, 8, 4, 15};
+        long expected = 15;
+        long actual = service.findMax(incomes);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMaxWithSingleElement() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {42};
+        long expected = 42;
+        long actual = service.findMax(incomes);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMaxWithAllEqual() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {7, 7, 7, 7};
+        long expected = 7;
+        long actual = service.findMax(incomes);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMaxWithNegativeNumbers() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {-5, -2, -10, -1};
+        long expected = -1;
+        long actual = service.findMax(incomes);
+        Assertions.assertEquals(expected, actual);
+
     }
 }
